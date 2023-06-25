955baf87f176a158b680c174fd9e4c53823fa21a
oddc <olivier.dedecker@gmail.com>
2023-06-25 13:03:15 +0200

    Initial commit

---
+++ envelope_strategy_live.py
@@ -0,0 +1,266 @@
+# ---------------------------
+# ENVELOPE STRATEGY LIVE CODE
+# ---------------------------
+
+# --- IMPORT LIBS ---
+import sys
+sys.path.append('/Users/olivierdedecker/Documents/00_Dev/Python/Crypto_Robot_live/live_tools/utilities')
+import ccxt
+import ta
+import pandas as pd
+#from perp_bitget import PerpBitget
+from perp_bybit import *
+from custom_indicators import get_n_columns
+from datetime import datetime
+import time
+import json
+import pprint
+import uuid
+
+
+# --- LAUNCH ---
+now = datetime.now()
+current_time = now.strftime("%d/%m/%Y %H:%M:%S")
+current_time_python = now.timestamp()
+print("--- Start Execution Time :", current_time, "---")
+
+
+# --- PARAMETERS & VARIABLES ---
+# -- Account --
+f = open('../live_tools/secret.json')
+secret = json.load(f)
+f.close()
+
+exchange_name = 'bybit'
+account_to_select = 'testnet_account'
+production = True
+
+# -- Coins & timeframe --
+timeframe = '1h'
+pair = "BTC/USDT:USDT"
+leverage = 1
+print(f"--- {pair} {timeframe} Leverage x {leverage} ---")
+
+# -- Indicator variable --
+ema_shifts = [0.01, 0.02, 0.03]
+ema_period = 20
+
+# -- Rules --
+SL_ativation = False                # flag to set explicit stop loss
+TP_activation = False               # flag to set explicit take profit
+SL_percentage = 0.5                 # stop loss percentage applied to buy/sell price if flag activated
+TP_percentage = 0.10                # not used
+multi_ATR = 0.3                     # used to set take profit if flag activated
+showLog = True                      #
+max_coins_in_position = 1           # max number of coin positions
+nLevel = len(ema_shifts)            # max number of open positions per coin
+position_type = ["long", "short"]
+open_position_asap = True
+close_position_with_indicator = False
+
+# -- Value initialisation of trade monitoring variables of each coin --
+stopLoss = 0            # list of stop loss of each coin, set if SL_ativation is True
+takeProfit = 5000000    # list of take profit if TP_activation is True
+walletCoinArray = 0     # list of number of coins longed / shorted
+walletUsdArray = 0      # list of USDT used to enter in the position of each coin
+entryPrice = 0          # list of entry price of each coin
+leverageEnter = 1       # list of leverage used for each coin
+position = 'LONG'       # list of position directions for each coin
+nPosition = 0           # list of number of positions in each coin
+activePositions = 0     # number of open positions
+
+# -- Formating of dataframes --
+pd.set_option('display.max_columns', 5)             # Display any number of columns
+pd.set_option('display.max_rows', 5)                # Display any number of rows
+pd.set_option('display.expand_frame_repr', False)   # Don't wrap repr(DataFrame) across additional lines
+
+
+# --- FUNCTIONS ---
+def open_long(row):
+    if ('long' in position_type) and open_position_asap:
+        return True
+    else:
+        return False
+
+def close_long(row):
+    if close_position_with_indicator:
+        # some code
+        return True
+    else:
+        return False
+
+def open_short(row):
+    if ('short' in position_type) and open_position_asap:
+        return True
+    else:
+        return False
+
+def close_short(row):
+    if close_position_with_indicator:
+        # some code
+        return True
+    else:
+        return False
+
+
+# --- INITIALIZE EXCHANGE & GET BALANCE ---
+# connect exchange
+bybit = PerpBybit(
+    apiKey=secret[account_to_select]["apiKey"],
+    secret=secret[account_to_select]["secret"],
+    default_type='swap',
+    is_real=secret[account_to_select]["is_real"]
+)
+
+# get portfolio balance data from exchange
+usdt_equity = float(bybit.get_usdt_equity())
+usdt_available_balance = float(bybit.get_usdt_available_balance())
+print(f'available usdt balance : {usdt_available_balance}')
+
+# get balance, position and order data
+usd_balance = float(bybit.get_usdt_equity())
+print("USD balance :", round(usd_balance, 2), "$")
+
+positions_data = bybit.get_open_position()
+position_list = [
+    {"side": d["side"], "size": float(d["contracts"]) * float(d["contractSize"]), "market_price":d["markPrice"], "usd_size": float(d["contracts"]) * float(d["contractSize"]) * float(d["markPrice"]), "open_price": d["entryPrice"]}
+    for d in positions_data if d["symbol"] == pair]
+df_position = pd.DataFrame(position_list)
+print('Positions')
+print(df_position)
+
+orders_list = []
+for order in bybit.get_open_orders():
+    orders_list.append(order["info"])
+df_orders = pd.DataFrame(orders_list)
+if df_orders.empty == False:
+    df_orders["price"] = pd.to_numeric(df_orders["price"])
+    df_orders["qty"] = pd.to_numeric(df_orders["qty"])
+print('Open orders')
+print(df_orders)
+
+# Get data
+df = bybit.get_more_last_historical_async(pair, timeframe, 1000)
+
+
+# --- POPULATE INDICATORS ---
+sell_ema_values={}
+buy_ema_values={}
+df.drop(columns=df.columns.difference(['open','high','low','close','volume']), inplace=True)
+
+df['ema_base'] = ta.trend.ema_indicator(close=df['close'], window=ema_period)
+for i, shift in enumerate(ema_shifts, start=1):
+    df[f'ema_high_{i}'] = df['ema_base'] * (1 + shift)
+    df[f'ema_low_{i}'] = df['ema_base'] * (1 - shift)
+    sell_ema_values[f'ema_high_{i}'] = bybit.convert_price_to_precision(pair, df.iloc[-1][f'ema_high_{i}'])
+    buy_ema_values[f'ema_low_{i}'] = bybit.convert_price_to_precision(pair, df.iloc[-1][f'ema_low_{i}'])
+
+# df = get_n_columns(df, ["ema_base"] + [f"ema_high_{i}" for i in range(1, len(ema_shifts) + 1)] 
+#                    + [f"ema_low_{i}" for i in range(1, len(ema_shifts) + 1)] + ["close"], 1)
+
+
+# --- CANCEL OPEN UNFILLED ORDERS ---
+cancelled_longs = []
+cancelled_shorts = []
+
+for order in orders_list:
+    if order['orderLinkId'] != '' and order['side']=='Buy':
+        enveloppe_id = order['orderLinkId'].split("#")[0]
+        cancelled_longs.append(enveloppe_id)
+        order_id = order['orderId']
+        order_symbol = order['symbol']
+        print(f'Cancel buy order {order_id}')
+        if production:
+            bybit.cancel_order(order_id = order_id, symbol=order_symbol)
+    if order['orderLinkId'] != '' and order['side']=='Sell':
+        enveloppe_id = order['orderLinkId'].split("#")[0]
+        cancelled_shorts.append(enveloppe_id)
+        order_id = order['orderId']
+        order_symbol = order['symbol']
+        print(f'Cancel sell order {order_id}')
+        if production:
+            bybit.cancel_order(order_id = order_id, symbol=order_symbol)
+
+print(f'Cancelled {len(cancelled_longs)} longs and {len(cancelled_shorts)} shorts')
+
+
+# --- CREATE AND MODIFY ORDERS ---
+# deal entry logic
+row = df.iloc[-2]
+unique_id = f"#{uuid.uuid4()}"
+
+# Determine order size
+available_positions = max_coins_in_position #- len(df_position)
+market_price = float(df.iloc[-1]['close'])
+usdt_position_size = usdt_available_balance / available_positions
+usdt_order_size = (usdt_position_size*leverage) / nLevel
+print(f'usdt order size for new orders: {usdt_order_size} using {leverage}x leverage')
+coin_order_size = usdt_order_size / market_price
+rounded_coin_order_size = float(bybit.convert_amount_to_precision(pair, coin_order_size))
+print(f'coin order size for new orders: {coin_order_size}')
+print(f'rounded coin order size for new orders: {rounded_coin_order_size}')
+
+# Adjust TP of open position
+if len(positions_data) > 0:
+    print(f"Active position")
+    for order in orders_list:
+        if order['orderLinkId']=='':
+            order_size = order['qty']
+            order_tp = bybit.convert_price_to_precision(pair, df.iloc[-1]['ema_base'])
+            print(f"Modify position TP: {order_size} {pair} at the price of {order_tp}$")
+            if production:
+                order = bybit.edit_order(
+                    id=order['orderId'],
+                    symbol=order['symbol'],
+                    type=order['orderType'],
+                    side=order['side'],
+                    amount=order['qty'],
+                    price=None,
+                    params={
+                        'triggerPrice': bybit.convert_price_to_precision(pair, df.iloc[-1]['ema_base'])
+                    }
+                )
+            # print(f'modified order:')
+            # pprint.pprint(order)
+    
+# Create new limit orders for remaining slots
+if open_long(row) and "long" in position_type:
+    for ema, ema_value in buy_ema_values.items():
+        if (ema in cancelled_longs) or df_orders.empty:
+            print(f"Place {ema} Long Limit Order: {rounded_coin_order_size} {pair} at the price of {ema_value}$")
+            if production:
+                order = bybit.place_limit_order(
+                    symbol=pair,
+                    side='buy',
+                    amount=rounded_coin_order_size,
+                    limit=ema_value,
+                    sl=None,
+                    tp=bybit.convert_price_to_precision(pair, df.iloc[-1]['ema_base']),
+                    reduce=False,
+                    orderLinkId=f'{ema}{unique_id}'
+                )          
+                #print('placed buy orders:')
+                #pprint.pprint(order)
+                
+if open_short(row) and "short" in position_type:
+    for ema, ema_value in sell_ema_values.items():
+        if (ema in cancelled_shorts) or df_orders.empty:
+            print(f"Place {ema} Short Limit Order: {rounded_coin_order_size} {pair} at the price of {ema_value}$")
+            if production:
+                order = bybit.place_limit_order(
+                    symbol=pair,
+                    side='sell',
+                    amount=rounded_coin_order_size,
+                    limit=ema_value,
+                    sl=None,
+                    tp=bybit.convert_price_to_precision(pair, df.iloc[-1]['ema_base']),
+                    reduce=False,
+                    orderLinkId=f'{ema}{unique_id}'
+                )          
+                #print('placed buy orders:')
+                #pprint.pprint(order)
+
+# --- CLOSE ---
+now = datetime.now()
+current_time = now.strftime("%d/%m/%Y %H:%M:%S")
+print("--- End Execution Time :", current_time, "---")
---
+++ perp_bybit.py
@@ -0,0 +1,321 @@
+import ccxt
+import pandas as pd
+import time
+from multiprocessing.pool import ThreadPool as Pool
+import numpy as np
+from datetime import datetime
+
+# --- INTERACT WITH BYBIT ---
+
+class PerpBybit():
+    def __init__(self, apiKey=None, secret=None, default_type=None, is_real=None):
+        bybit_auth_object = {
+            "apiKey": apiKey,
+            "secret": secret,
+            'options': {
+                'defaultType': default_type,
+            }
+        }
+        if bybit_auth_object['secret'] == None:
+            self._auth = False
+            self._session = ccxt.bybit()
+        else:
+            self._auth = True
+            self._session = ccxt.bybit(bybit_auth_object)
+            if is_real=="False":
+                print('hey .. set sandbox mode ON')
+                self._session.set_sandbox_mode(True)
+            else:
+                print('ok.. set sandbox mode OFF')
+                self._session.set_sandbox_mode(False)
+        self.market = self._session.load_markets()
+
+    def authentication_required(fn):
+        """Annotation for methods that require auth."""
+        def wrapped(self, *args, **kwargs): #!!!!!!!!!
+            if not self._auth:
+                # print("You must be authenticated to use this method", fn)
+                raise Exception("You must be authenticated to use this method")
+            else:
+                return fn(self, *args, **kwargs)#!!!! *kwargs
+        return wrapped
+    
+    def activate_testnet_mode(self):
+        self._session.set_sandbox_mode(True) # activates testnet mode
+
+#   market data
+
+    def get_last_historical(self, symbol, timeframe, limit):
+        result = pd.DataFrame(data=self._session.fetch_ohlcv(
+            symbol, timeframe, None, limit=limit))
+        result = result.rename(
+            columns={0: 'timestamp', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'})
+        result = result.set_index(result['timestamp'])
+        result.index = pd.to_datetime(result.index, unit='ms')
+        del result['timestamp']
+        return result
+
+    def get_more_last_historical_async(self, symbol, timeframe, limit):
+        max_threads = 4
+        pool_size = round(limit/100)  # your "parallelness"
+
+        # define worker function before a Pool is instantiated
+        full_result = []
+        def worker(i):
+
+            try:
+                return self._session.fetch_ohlcv(
+                symbol, timeframe, round(time.time() * 1000) - (i*1000*60*60), limit=100)
+            except Exception as err:
+                raise Exception("Error on last historical on " + symbol + ": " + str(err))
+
+        pool = Pool(max_threads)
+
+        full_result = pool.map(worker,range(limit, 0, -100))
+        full_result = np.array(full_result).reshape(-1,6)
+        result = pd.DataFrame(data=full_result)
+        result = result.rename(
+            columns={0: 'timestamp', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'})
+        result = result.set_index(result['timestamp'])
+        result.index = pd.to_datetime(result.index, unit='ms')
+        del result['timestamp']
+        return result.sort_index()
+
+#   account data
+
+    @authentication_required
+    def get_usdt_equity(self):
+        try:
+            usdt_equity = self._session.fetch_balance({'coin':'USDT' })["info"]['result']['list'][0]['equity']
+
+        except BaseException as err:
+            raise Exception("An error occured", err)
+        try:
+            return usdt_equity
+        except:
+            return 0
+
+    @authentication_required
+    def get_open_position(self,symbol=None):
+        #print("ohoho",self._session.fetch_positions(symbol))
+        try:
+            positions = self._session.fetch_positions(symbol)
+            #print('hello', positions)
+            truePositions = []
+            for position in positions:
+                if float(position['contractSize']) > 0:
+                    truePositions.append(position)
+            return truePositions
+        except BaseException as err:
+            raise TypeError("An error occured in get_open_position", err)
+    
+    @authentication_required
+    def get_open_orders(self,symbol=None):
+        try:
+            open_orders = self._session.fetch_open_orders(symbol)
+
+        except BaseException as err:
+            raise Exception("An error occured", err)
+        try:
+            return open_orders
+        except:
+            return 0
+
+    @authentication_required
+    def get_all_balances(self):
+        try:
+            balances = self._session.fetch_balance()
+
+        except BaseException as err:
+            raise Exception("An error occured", err)
+        try:
+            return balances
+        except:
+            return 0
+        
+    @authentication_required
+    def get_usdt_available_balance(self):
+        try:
+            usdt_avail_balance = self._session.fetch_balance({'coin':'USDT' })["info"]['result']['list'][0]['availableBalance']
+
+        except BaseException as err:
+            raise Exception("An error occured", err)
+        try:
+            return usdt_avail_balance
+        except:
+            return 0
+
+    @authentication_required
+    def get_orders(self, symbol, since=None):
+        try:
+            open_orders = self._session.fetch_orders(symbol=symbol, since=since)
+
+        except BaseException as err:
+            raise Exception("An error occured", err)
+        try:
+            return open_orders
+        except:
+            return 0
+
+#   trade
+
+    def convert_amount_to_precision(self, symbol, amount):
+        return self._session.amount_to_precision(symbol, amount)
+
+    def convert_price_to_precision(self, symbol, price):
+        return self._session.price_to_precision(symbol, price)
+
+    @authentication_required
+    def place_market_order(self, symbol, side, amount, reduce=False):
+        try:
+            return self._session.create_order(
+                symbol,
+                'market',
+                side,
+                self.convert_amount_to_precision(symbol, amount),
+                None,
+                params = {'reduce_only': reduce},
+            )
+        except BaseException as err:
+            raise Exception(err)
+        
+    @authentication_required
+    def place_limit_order(self, symbol, side, amount, limit, sl=None, tp=None, reduce=False, orderLinkId=None):
+        try:
+            return self._session.create_order(
+                symbol,
+                'limit',
+                side,
+                self.convert_amount_to_precision(symbol, amount),
+                self.convert_amount_to_precision(symbol,limit),
+                params = {
+                    'reduce_only': reduce,
+                    'takeProfit': self.convert_amount_to_precision(symbol,tp),
+                    'orderLinkId': orderLinkId
+                    },
+            )
+        except BaseException as err:
+            raise Exception(err)
+
+    @authentication_required
+    def cancel_order(self, order_id, symbol, params={}):
+        try:
+            return self._session.cancel_order(
+                order_id,
+                symbol,
+                params
+            )
+        except BaseException as err:
+            raise Exception(err)
+
+    
+    @authentication_required
+    def edit_order(self, id, symbol, type, side, amount=None, price=None, params={}):
+        try:
+            return self._session.edit_order(
+                id=id,
+                symbol=symbol,
+                type=type,
+                side=side,
+                amount=amount,
+                price=price,
+                params=params
+            )
+        except BaseException as err:
+            raise Exception(err)
+    
+    
+    
+    @authentication_required
+    def place_market_stop_loss(self, symbol, side, amount, trigger_price, reduce=False):
+
+        try:
+            return self._session.create_order(
+                symbol,
+                'market',
+                side,
+                self.convert_amount_to_precision(symbol, amount),
+                self.convert_price_to_precision(symbol, trigger_price),
+                params = {
+                    'stop_loss': self.convert_price_to_precision(symbol, trigger_price),  # your stop price
+                    "triggerType": "market_price",
+                    "reduce_only": reduce
+                },
+            )
+        except BaseException as err:
+            raise Exception(err)
+
+        
+
+# --- CREATE DATAFRAMES ---
+
+def create_filled_orders_df(orders):
+    filled_orders_list = []
+    for order in orders:
+        created_ts = float(order['info']['createdTime'])/1000
+        updated_ts = float(order['info']['updatedTime'])/1000
+        order_data = {
+            'order_id': order['info']['orderId'],
+            'symbol': order['info']['symbol'],
+            'avg_price': order['info']['avgPrice'],
+            'quantity': order['info']['qty'],
+            'side': order['info']['side'],
+            'order_type': order['info']['orderType'],
+            'stop_order_type': order['info']['stopOrderType'] if order['info']['stopOrderType'] != 'UNKNOWN' else '-',
+            'limit_price': order['info']['price'],
+            'sl_price':order['info']['stopLoss'],
+            'tp_price':order['info']['takeProfit'],
+            'status': order['info']['orderStatus'],
+            'created_ts': datetime.fromtimestamp(created_ts, tz = None),
+            'updated_ts': datetime.fromtimestamp(updated_ts, tz = None)
+        }
+        if order['info']['orderStatus']=='Filled':
+            filled_orders_list.append(order_data)
+    df_filled_orders = pd.DataFrame(filled_orders_list)
+    return df_filled_orders
+
+def create_open_orders_df(orders):
+    open_orders_list = []
+    for order in orders:
+        created_ts = float(order['info']['createdTime'])/1000
+        updated_ts = float(order['info']['updatedTime'])/1000
+        order_data = {
+            'order_id': order['info']['orderId'],
+            'symbol': order['info']['symbol'],
+            'avg_price': order['info']['avgPrice'],
+            'quantity': order['info']['qty'],
+            'side': order['info']['side'],
+            'order_type': order['info']['orderType'],
+            'stop_order_type': order['info']['stopOrderType'] if order['info']['stopOrderType'] != 'UNKNOWN' else '-',
+            'limit_price': order['info']['price'],
+            'sl_price':order['info']['stopLoss'],
+            'tp_price':order['info']['takeProfit'],
+            'status': order['info']['orderStatus'],
+            'created_ts': datetime.fromtimestamp(created_ts, tz = None),
+            'updated_ts': datetime.fromtimestamp(updated_ts, tz = None)
+        }
+        open_orders_list.append(order_data)
+    df_open_orders = pd.DataFrame(open_orders_list)
+    return df_open_orders
+
+def create_open_positions_df(positions):
+    open_positions_list=[]
+    for position in positions:
+        created_ts = float(position['info']['createdTime'])/1000
+        updated_ts = float(position['info']['updatedTime'])/1000
+        order_data = {
+            'position_id': position['info']['positionIdx'],
+            'symbol': position['info']['symbol'],
+            'avg_price': position['info']['avgPrice'],
+            'size': position['info']['size'],
+            'side': position['info']['side'],
+            'leverage': position['info']['leverage'],
+            'status': position['info']['positionStatus'],
+            'created_ts': datetime.fromtimestamp(created_ts, tz = None),
+            'updated_ts': datetime.fromtimestamp(updated_ts, tz = None)
+        }
+        open_positions_list.append(order_data)
+    df_open_positions = pd.DataFrame(open_positions_list)
+    return df_open_positions
+
+
