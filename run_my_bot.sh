4260fa653750e8b420372e74239413414008d17d
Olivier De Decker <olivier.dedecker@gmail.com>
2023-08-03 22:59:38 +0200

    Delete run_my_bot.sh

--- run_my_bot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#source /home/oddc/miniconda3/bin/conda
-#conda activate crypto_robot_live
-python3 /home/oddc/Crypto_Robot_live/crypto_robot_live/my_code/my_strategies_live/envelope_strategy_live.py
